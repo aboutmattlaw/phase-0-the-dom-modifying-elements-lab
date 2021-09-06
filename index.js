3188642f717f4fb8f06d4f0225cb17f543f3e9d9
Matt <aboutmattlaw@gmail.com>
2021-09-06 15:10:46 -0400

    redo

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-// Write your code here!
+const main = document.getElementById("main")
 main.remove()
+
+
 const newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
 newHeader.id = 'victory'
-document.body.appendChild(newHeader)
-newHeader.id = 'victory'
-newHeader.innerHTML = "Matt is the champion"
+newHeader.textContent = "Matt is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Attempt
+
+
+// // Write your code here!
+// main.remove()
+// const newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+// newHeader.id = 'victory'
+// document.body.appendChild(newHeader)
+// newHeader.id = 'victory'
+// newHeader.innerHTML = "Matt is the champion"
